6826dcb697032694da31b7ab163a3d9af0ad0424
İbrahim Telman <cheriorn@gmail.com>
2018-03-29 00:20:14 +0300

    Youtube search parameters added

---
+++ index.js
@@ -0,0 +1,28 @@
+var axios = require('axios');
+
+var ROOT_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+module.exports = function (options, callback) {
+  if (!options.key) {
+    throw new Error('Youtube Search expected key, received undefined');
+  }
+
+  var params = Object.assign({}, {
+    part: 'snippet',
+    key: '',
+    q: '',
+    type: 'video',
+    maxResults: 10,
+    order: "relevance",
+    pageToken: "",
+    videoDuraation: ""
+  }, options);
+
+  axios.get(ROOT_URL, { params: params })
+    .then(function(response) {
+      if (callback) { callback(response.data.items); }
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+};
